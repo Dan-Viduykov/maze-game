5a4a7edb68e56aeeb57fc7b93b7116263046606d
Danya_Vid <danilviduykov@gmail.com>
2022-08-06 18:48:24 +0500

    add adaptive

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowLeft, faArrowRight, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+const arrowUp = <FontAwesomeIcon icon={faArrowUp} /> 
+const arrowRight = <FontAwesomeIcon icon={faArrowRight} /> 
+const arrowDown = <FontAwesomeIcon icon={faArrowDown} /> 
+const arrowLeft = <FontAwesomeIcon icon={faArrowLeft} /> 
+
+export {arrowUp, arrowRight, arrowDown, arrowLeft}
